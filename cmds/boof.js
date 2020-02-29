fe544f16617385f5a3499006f2477818d3185de9
PumpkinmanTV <pumpkinman.twitch@gmail.com>
2020-02-28 22:23:24 -0600

    v0.0.3

--- cmds/boof.js
+++ cmds/boof.js
@@ -1,5 +1,5 @@
 module.exports.run = async (client, msg, args) => {
-	msg.channel.send(`${msg.author} is about to boof hard, come and join them!`);
+	msg.channel.send(`@here ${msg.author} is about to boof hard, come and join them!`);
 	var fs = require('fs');
 	var boofFiles = fs.readdirSync('./img/boof/')
 	let chosenFile = boofFiles[Math.floor(Math.random() * boofFiles.length)]

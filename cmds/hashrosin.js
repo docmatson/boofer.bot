6dc5d64270433ebd1c0539d2d57b4dba7440765d
Docmatson <garyrocket19@gmail.com>
2020-08-14 21:53:22 -0600

    added start.sh and hash.jpg

--- cmds/hashrosin.js
+++ cmds/hashrosin.js
@@ -1,5 +1,5 @@
 module.exports.run = async (client, msg, args) => {
-	msg.channel.send("https://media.discordapp.net/attachments/447859236433690644/618436539780694028/17991125_10156112914543345_4818480232329836675_n.jpg Repent Sinner, Smoke the Fine Hash Rosin like the Lord Commands Thee @here");
+	msg.channel.send( {files: [`./img/meme/hash.jpg`]} )
 }
 
 module.exports.help = {

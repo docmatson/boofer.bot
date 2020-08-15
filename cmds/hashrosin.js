65b5398fe782dc7fbfcc5a0f3b0f621345e9231a
Docmatson <garyrocket19@gmail.com>
2020-08-14 22:17:21 -0600

    fixed hashrosin.js

--- cmds/hashrosin.js
+++ cmds/hashrosin.js
@@ -1,5 +1,9 @@
 module.exports.run = async (client, msg, args) => {
-	msg.channel.send( {files: [`./img/meme/hash.jpg`]} )
+	var fs = require('fs');
+	var hashFiles = fs.readdirSync('./img/hash/')
+	let chosenFile = hashFiles[Math.floor(Math.random() * hashFiles.length)]
+
+	msg.channel.send( {files: [`./img/hash/${chosenFile}`]} )
 }
 
 module.exports.help = {
